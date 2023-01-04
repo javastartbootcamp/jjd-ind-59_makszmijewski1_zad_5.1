a8f7310b64d20b79d1a1c570d72ce1a154bed49f
Maksymilian Żmijewski <makszmijewski1@gmail.com>
2023-01-04 16:41:06 +0100

    pierwszy commit

--- src/main/java/pl/javastart/task/CoordinateSystem.java
+++ src/main/java/pl/javastart/task/CoordinateSystem.java
@@ -4,8 +4,29 @@ import java.util.Scanner;
 
 public class CoordinateSystem {
 
-    // uzupełnij metodę. Używaj scannera przekazanego w parametrze.
     void run(Scanner scanner) {
-        System.out.println("Punkt (5, -3)");
+        System.out.println("Podaj X");
+        int X = scanner.nextInt();
+        System.out.println("Podaj Y");
+        int Y = scanner.nextInt();
+        System.out.println("Punkt (" + X + ", " + Y + ") "
+                + whichQuarter(X, Y));
+    }
+
+    public String whichQuarter(int X, int Y) {
+        if (X == 0 && Y == 0)
+            return "leży na środku układu współrzędnych";
+        else if (Y == 0)
+            return "leży na osi X";
+        else if (X == 0)
+            return "leży na osi Y";
+        else if (X > 0 && Y > 0)
+            return "leży w I ćwiartce układu współrzędnych";
+        else if (X < 0 && Y > 0)
+            return "leży w II ćwiartce układu współrzędnych";
+        else if (X < 0 && Y < 0)
+            return "leży w III ćwiartce układu współrzędnych";
+        else
+            return "leży w IV ćwiartce układu współrzędnych";
     }
 }
--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         CoordinateSystem coordinateSystem = new CoordinateSystem();
         Scanner scanner = new Scanner(System.in);
         coordinateSystem.run(scanner);
